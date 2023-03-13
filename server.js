087d842646eada5c28e83463b6e2c95c6e0400ea
shankareshbb <shankaresh@mollatech.com>
2023-03-13 16:19:24 +0530

    cleared

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const serveIndex=require('serve-index')
-app.use(express.static(path.join(__dirname, "build")));
-app.use('/.well-known', express.static('.well-known'), serveIndex('.well-known'));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-app.listen(8886,() =>{console.log("IRI Sample App Running on 8886")})
+app.use(express.static(path.join(__dirname)));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(5464,() =>{console.log("IRI Sample App Running on 5464")})
